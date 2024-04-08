b039618180784032a93d0c42417859d75bd90908
saami mahmud <saamimahmud123@gmail.com>
2024-04-09 01:38:35 +0530

    first commit

---
+++ class_creation.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class Student{
+    char name[20];
+    int age;
+    public :
+    void get(){
+        cout<<"Enter the name of the student :";
+        cin>>name;
+        cout<<"Enter the age :";
+        cin>>age;
+    }
+    void display(){
+        cout<<"Name : "<<name<<"\n";
+        cout<<"Age : "<<age;
+    }
+
+};
+int main()
+{
+    Student S1;
+    S1.get();
+    S1.display();
+return 0;
+}
