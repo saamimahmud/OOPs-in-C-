638fcc4e0cb35a51f8b4e0ea51d83b1a4168f7e3
saami mahmud <saamimahmud123@gmail.com>
2024-04-09 16:38:03 +0530

    second commit

--- member_func_with_parameters.cpp
+++ member_func_with_parameters.cpp
@@ -1,16 +1,17 @@
 #include<iostream>
-#include<string.h>
+#include<string>
 using namespace std;
 class Student{
     public:
-    void disp(int);
+    void disp(int,string);
 };
-void Student::disp(int rollno)
+void Student::disp(int rollno, string name)
 {
-    cout<<"The Roll number of the student is : "<<rollno;
+    cout<<"The Roll number of the student is : "<<rollno<<endl;
+    cout<<"The Name of the student is : "<<name;
 }
 int main(){
     Student S1;
-    S1.disp(37);
+    S1.disp(37,"Saami");
     return 0;
 }
