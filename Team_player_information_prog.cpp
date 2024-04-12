fbb2f125578e6ea609d18aa31956aa8edce6721a
saami mahmud <saamimahmud123@gmail.com>
2024-04-13 02:01:45 +0530

    changed char to const char and added menu

--- Team_player_information_prog.cpp
+++ Team_player_information_prog.cpp
@@ -1,4 +1,7 @@
 #include<iostream>
+#include<string>
+#include<cstdlib>
+#include<string.h>
 using namespace std;
 class Player{
     int age;
@@ -18,13 +21,11 @@ class Player{
         cout<<"The age of the player is : "<<age<<endl;
         cout<<"The name of the team is : "<<team<<endl;
     }
-    void change(){
-        cout<<"Name : "<<endl;
-        cin>>name;
-        cout<<"Age : "<<endl;
-        cin>>age;
-        cout<<"Team Name : "<<endl;
-        cin>>team;}
+    void change(const char n[40],const char t[40],int a){
+        strcpy(name,n);
+        strcpy(team,t);
+        age=a;
+    }
     void input(){
         cout<<"Name of the player : "<<endl;
         cin>>name;
@@ -38,8 +39,33 @@ class Player{
 int main(){
     Player p1;
     p1.display();
-
-    return 0;
+    int choice;
+    do{
+        cout<<"********MAIN MENU********"<<endl;
+        cout<<"1:- Take information of player "<<endl;
+        cout<<"2:- Display the information of player "<<endl;
+        cout<<"3:- change the information"<<endl;
+        cout<<"4:- EXIT "<<endl;
+        cout<<"Enter the number to take action : "<<endl;
+        cin>>choice;
+        switch(choice){
+            case 1:
+                p1.input();
+                break;
+            case 2:
+                p1.display();
+                break;
+            case 3:
+                p1.change("Saami","barcelona",22);
+                break;
+            case 4:
+            exit(0);
+                break;
+            default:
+                cout<<"please enter number fromm the following (1,2,3,4) "<<endl;
+                        }
+     } while(1);
+     return 0;
+}
 
     
-}
