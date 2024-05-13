66401c13d52bd4178fbee6d0039f64972fd50d4a
saamimahmud <saamimahmud123@gmail.com>
2024-05-13 23:54:50 +0530

    first commit

---
+++ operator_overloading.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<string>
+#include<string.h>
+using namespace std;
+class add_name{
+    string name="saami";
+    public:
+    add_name() {}
+    add_name(string one){
+        name=one;
+    }
+    void get(){
+        cout<<"enter name : "<<endl;
+        cin>>name;
+    }
+    void show(){
+        cout<<name;
+
+    }
+    add_name operator +(add_name firstname){
+        add_name fullname;
+        fullname.name=firstname.name+name;
+        return fullname;
+
+    }
+    int main(){
+        add_name firstname("Mahud");
+        
+        add_name Full_name=firstname + name;
+        Full_name.show();
+        return 0;
+
+    }
+
+
+};
+
